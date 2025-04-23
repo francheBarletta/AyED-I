601c2bd55756c2c50a7c32fca106ab1fdf5e24e9
Franche <franchebarletta@gmail.com>
2025-04-23 17:58:02 -0300

    Ejecution & test

---
+++ proyect2-2025.hs
@@ -0,0 +1,115 @@
+--Ejercicio 1
+
+data Carrera = Matematica | Fisica | Computacion | Astronomia deriving (Eq)
+
+titulo :: Carrera -> String
+titulo Matematica  = "Licenciatura en Matematica"
+titulo Fisica      = "Licenciatura en Fisica"
+titulo Computacion = "Licenciatura en Computacion"
+titulo Astronomia  = "Licenciatura en Astronomia"
+
+data NotaBasica = Do | Re | Mi | Fa | Sol | La | Si deriving (Eq)
+
+cifradoAmericano :: NotaBasica -> Char
+cifradoAmericano Do  = 'C'
+cifradoAmericano Re  = 'D'
+cifradoAmericano Mi  = 'E'
+cifradoAmericano Fa  = 'F'
+cifradoAmericano Sol = 'G'
+cifradoAmericano La  = 'A'
+cifradoAmericano Si  = 'B'
+
+--Ejercicio 3
+
+--a)
+minimoElemento :: (Ord a) => [a] -> a
+minimoElemento [] = error "La lista no puede estar vacia"
+minimoElemento [x] = x
+minimoElemento (x:xs) = min x (minimoElemento xs)
+
+--minimoElemento [1,2,3]
+--1
+
+--b)
+minimoElemento' :: (Ord a, Bounded a) => [a] -> a 
+minimoElemento' [] = maxBound
+minimoElemento' [x] = x
+minimoElemento' (x:xs) = min x (minimoElemento' xs)
+
+--Ejercicio 3
+
+--a)
+--Sinonimos de tipo
+type Altura = Int
+type NumCamiseta = Int
+
+--Tipos algebraicos sin parametros (aka enumerado)
+data Zona = Arco | Defensa | Mediocampo | Delantera deriving (Eq)
+data TipoRevez = DosManos | UnaMano
+data Modalidad = Carrera | Pista | Monte | BMX
+data PiernaHabil =  Izquierda | Derecha
+
+--Sinonimo
+type ManoHabil = PiernaHabil
+
+--Deportista es un tipo algebraico con constructores parametricos
+data Deportista = Ajedresista                                    -- Constructor sin argumentos
+                | Ciclista Modalidad                             -- Constructor con un argumento
+                | Velocista Altura                               -- Constructor con un argumento
+                | Tenista TipoRevez ManoHabil Altura             -- Constructor con tres argumentos
+                | Futbolista Zona NumCamiseta PiernaHabil Altura -- Constructor con cuatro argumentos
+
+
+--c)
+contar_velocistas :: [Deportista] -> Int
+contar_velocistas [] = 0
+contar_velocistas (Velocista _ : xs) = 1 + contar_velocistas xs
+contar_velocistas (_ : xs) = contar_velocistas xs
+
+--Main> let lista = [Velocista 175, Ciclista Pista, Ajedresista, Velocista 180, Futbolista Delantera 10 Izquierda 185]
+--Main> contar_velocistas lista
+--2
+
+--d)
+contar_futbolistas :: [Deportista] -> Zona -> Int
+contar_futbolistas [] _ = 0
+contar_futbolistas (Futbolista zona _ _ _ : xs) z
+  | zona == z  = 1 + contar_futbolistas xs z
+  | otherwise  = contar_futbolistas xs z
+contar_futbolistas (_: xs) z = contar_futbolistas xs z
+
+--ghci> let lista = [Futbolista Arco 1 Derecha 180, Futbolista Defensa 2 Izquierda 175, Ciclista Pista, Ajedresista]
+--ghci> let zona = Arco
+--ghci> contar_futbolistas lista zona
+--1
+
+--5)
+--a)
+
+sonidoNatural :: NotaBasica -> Int
+sonidoNatural Do = 0
+sonidoNatural Re = 2
+sonidoNatural Mi = 4
+sonidoNatural Fa = 5
+sonidoNatural Sol = 7
+sonidoNatural La = 9
+sonidoNatural Si = 11
+
+--b)
+
+data Alteracion = Bemol | Natural | Sostenido deriving (Show, Eq)
+
+--c)
+
+data NotaMusical = Nota NotaBasica Alteracion deriving (Eq)
+
+--d)
+
+sonidoCromatico :: NotaMusical -> Int
+sonidoCromatico (Nota _ Sostenido) = 1
+sonidoCromatico (Nota _ Bemol) = -1
+sonidoCromatico (Nota _ Natural) = 0
+
+--ghci> let nota = Nota Do Sostenido
+--ghci> sonidoCromatico nota
+--1
