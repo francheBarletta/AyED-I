6c5c2f5dcc26444d5a2fcef7c655733dd2c81a27
Tu Nombre <tu-email@ejemplo.com>
2025-04-28 12:14:50 -0300

    exam-H file

--- exam-H.hs
+++ exam-H.hs
@@ -27,3 +27,55 @@ premioBna = Reintegro "BnaMas" "Premio Cumplidor" 3000
 esReintegro :: Ahorro -> String -> Bool
 esReintegro (Reintegro billetera _ _) nombre = billetera == nombre
 esReintegro _ _ = False
+
+--gchi> esReintegro supermercadoUa "Uala"
+--False
+--ghci> esReintegro premioBna "BnaMas"
+--True
+
+--d)
+
+esDescuentoPrimavera :: Ahorro -> Bool
+esDescuentoPrimavera (Descuento _ _ _ mes) = mes == 9 || mes == 10 || mes == 11
+esDescuentoPrimavera _ = False
+
+--ghci> esDescuentoPrimavera supermercadoUa
+--True
+--ghci> esDescuentoPrimavera premioBna
+--False
+--ghci> esDescuentoPrimavera (Reintegro "BnaMas" "Premio Cumplidor" 3000)
+--False
+
+--e)
+
+totalDescuentosMes :: [Ahorro] -> Int -> Int
+totalDescuentosMes [] _ = 0
+totalDescuentosMes (Descuento _ _ monto mes : xs) mesBuscardo
+    | mes == mesBuscardo = monto + totalDescuentosMes xs mesBuscardo
+    | otherwise = totalDescuentosMes xs mesBuscardo
+
+--ghci> totalDescuentosMes [supermercadoUa, premioBna] 10
+--1500
+--ghci> totalDescuentosMes [supermercadoUa, premioBna] 11
+--0
+--ghci> totalDescuentosMes [supermercadoUa, premioBna] 9
+--0
+
+--2)
+
+data ColaAhorro = NoHayAhorro | HayAhorro Ahorro ColaAhorro
+    deriving (Show, Eq)
+
+ahorrosJuan :: ColaAhorro
+ahorrosJuan = AgregarAhorro supermercadoUa (AgregarAhorro premioBna NoHayAhorro)
+
+buenosAhorros :: ColaAhorro -> ColaAhorro
+buenosAhorros NoHayAhorro = NoHayAhorro
+buenosAhorros (AgregarAhorro ahorro cola)
+    | esReintegro ahorro "BnaMas" = AgregarAhorro ahorro (buenosAhorros cola)
+    | otherwise = buenosAhorros cola
+    
+--ghci> buenosAhorros ahorrosJuan
+--HayAhorro (Reintegro "BnaMas" "Premio Cumplidor" 3000) NoHayAhorro
+--ghci> buenosAhorros (AgregarAhorro supermercadoUa NoHayAhorro)
+--NoHayAhorro
