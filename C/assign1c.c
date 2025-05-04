a571fcab9f2b800bec96be5403ef94a88e00475b
Francesco Antonio Barletta <franchebarletta@gmail.com>
2025-05-04 17:14:19 -0300

    assign1c

---
+++ C/assign1c.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main () {
+    int x, y;
+
+    printf("Ingrese los valores de x, y (estado σ₀): ");
+    scanf("%d %d", &x, &y);
+
+    y = y + y;
+
+    printf("Estado σ₁: x = %d\n, y = %d\n", x, y);
+
+    x = x + y;
+
+    printf("Estado σ₂: x = %d\n, y = %d\n", x, y);
+
+    return 0;
+
+}
+
+/*
+Ingrese los valores de x, y (estado σ₀): 2 5
+Estado σ₁: x = 2, y = 10
+Estado σ₂: x = 12, y = 10
+*/
