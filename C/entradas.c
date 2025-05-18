dddd5a66594288055333ef3d4d66a41f3bf8a8d5
Francesco Antonio Barletta <franchebarletta@gmail.com>
2025-05-18 17:02:10 -0300

    entradas.c program

---
+++ C/entradas.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int pedir_entero(char name) {
+    int valor;
+    printf("Ingrese un entero'%c': ", name);
+    scanf("%d", &valor);
+    return valor;
+}
+
+void imprimir_entero(char name, int x) {
+    printf("El valor almacenado en la variable '%c' es: %d\n", name, x);    
+}
+
+int main () {
+    int n;
+
+    n = pedir_entero('n');
+
+    imprimir_entero('n', n);
+
+    return 0;
+}
+
