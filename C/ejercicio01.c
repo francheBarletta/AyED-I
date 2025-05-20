bde7e8f48005a4cf6b6432a525c935febc217671
Tu Nombre <franchebarletta@gmail.com>
2025-05-19 21:42:58 -0300

    first while, first assert

---
+++ C/ejercicio01.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <assert.h>
+
+
+void hola_hasta(int n) {
+    int i = 0;
+    while(i < n) {
+        printf("Hola\n");
+        i++;
+    }
+}
+    
+int main() {
+    int x;
+    
+    printf("Ingrese la cantidad de Hola's: ");
+    scanf("%d", &x);
+
+    assert (x > 0);
+
+    hola_hasta(x);
+
+    return 0;
+}
+
+/*
+Ingrese la cantidad de Hola's: 5
+Hola
+Hola
+Hola
+Hola
+Hola
+*/
