86a51746e0c04506b28cf95251c7ceb77b15c124
Tu Nombre <franchebarletta@gmail.com>
2025-05-20 19:25:52 -0300

    factorial :)

---
+++ C/producto_hasta.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <assert.h>
+
+int producto_hasta(int n) {
+    int i = 1;
+    int producto = 1;
+
+    while(i <= n) {
+        producto = producto * i;
+        i++;
+    }
+return producto;
+}
+
+int main() {
+    int n;
+    printf("Ingrese un numero positivo: ");
+    scanf("%d", &n);
+
+    assert(n > 0);
+    printf("El resultado del producto es: %d\n", producto_hasta(n));
+
+    return 0;
+}
+
+/*
+Ingrese un numero positivo: 5
+El resultado del producto es: 120
+*/
