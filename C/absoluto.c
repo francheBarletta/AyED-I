079b282d9cfe950efdd7ab0782d1ae3d53f15197
Tu Nombre <franchebarletta@gmail.com>
2025-05-19 21:47:52 -0300

    absoluto.c

---
+++ C/absoluto.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <assert.h>
+
+int main() {
+    int x;
+    printf("Ingrese un numero: ");
+    scanf("%d", &x);
+
+    assert(1);
+
+    if (x < 0) {
+        x = -x;
+        printf("El valor absoluto es: %d\n", x);
+    }
+    
+    if (x > 0) {
+        printf("El valor absoluto es: %d\n", x);
+    }
+    return 0;
+}
+
+/*
+Ingrese un numero: -4
+El valor absoluto es: 4
+Ingrese un numero: 6
+El valor absoluto es: 6
+*/
