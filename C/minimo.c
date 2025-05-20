01f5c0bc3291f205808fcc585d64a93625e91e2d
Tu Nombre <franchebarletta@gmail.com>
2025-05-19 21:48:29 -0300

    minimo.c file

---
+++ C/minimo.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <assert.h>
+
+int main () {
+    int x, y, m;
+
+    printf("Ingrese dos enteros: ");
+    scanf("%d %d", &x, &y);
+
+    assert(1);
+
+    if(x <=  y) {
+        m = x;
+    } else {
+        m = x;
+    }
+
+    assert((m == x || m == y) & m <= x && m <= y);
+
+    printf("El minimo es: %d\n", m);
+
+    return 0;
+}
+
+/*
+Ingrese dos enteros: 2 3
+El minimo es: 2
+*/
