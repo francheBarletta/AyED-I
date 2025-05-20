356b7109012a51565eaa47ee5a2bc2be0e99c1f9
Tu Nombre <franchebarletta@gmail.com>
2025-05-19 21:47:03 -0300

    intercambio.c

---
+++ C/intercambio.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <assert.h>
+
+int main(){
+    int x, y, z, temp;
+    printf("Ingrese los valores de x, y, z: ");
+    scanf("%d %d %d", &x, &y, &z);
+
+    int temp_x = x;
+    int temp_y = y;
+
+    assert(temp_x == x && temp_y == y);
+
+    z = x;
+    x = y;
+    y = z;
+
+    printf("Despues del intercambio: \n");
+    printf("x = %d\n", x);
+    printf("y = %d\n", y);
+    printf("z = %d\n", z);
+
+    return 0;
+}
+/*
+Ingrese los valores de x, y, z: 1 2 3
+Despues del intercambio: 
+x = 2
+y = 1
+z = 1
+*/
